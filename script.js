3f2362e944ca1db20ee16877ae742b20f1d6788d
Easyload2 <135276859+Easyload2@users.noreply.github.com>
2023-06-07 13:34:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+//STICK
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
+//slyder
+
+// Seleccionar el elemento con id="bodyfake"
+const body = document.getElementById('bodyfake');
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach((slide) => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
+
+//
